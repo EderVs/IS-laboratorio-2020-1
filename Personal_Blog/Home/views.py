b76c423c55c2f0b6cad46482b0a3a8be6fea3eb2
edervs <eedervs.97@gmail.com>
2019-09-01 22:18:49 -0500

    Adding templates and statics in Home Also changing to class-based views

--- Personal_Blog/Home/views.py
+++ Personal_Blog/Home/views.py
@@ -1,9 +1,44 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
 
 
+# Function Views
 def index(request):
     """
-        Index in my Web Page
+        Index in my Web Page.
     """
-    return HttpResponse("Hello! This is the Eder's Personal Blog")
+    print(request.method)
+    template = 'Home/index.html'
+    context = {}
+    return render(request, template, context)
+
+
+# Class-based Views
+class Index(View):
+    """
+        Index in my Web Page but with Clased based views.
+    """
+    template = 'Home/index.html'
+    context = {'title': 'Index'}
+
+    def get(self, request):
+        """
+            Get in my Index.
+        """
+        print('With class-based views')
+        return render(request, self.template, self.context)
+
+
+class About(View):
+    """
+        About me page.
+    """
+    template = 'Home/about.html'
+    context = {'title': 'About me'}
+
+    def get(self, request):
+        """
+            Get in About me.
+        """
+        return render(request, self.template, self.context)
