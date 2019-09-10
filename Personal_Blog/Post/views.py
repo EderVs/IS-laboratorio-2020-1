a9e89a7937d951a1f8c8f195b2119f52d8f7e1fa
edervs <eedervs.97@gmail.com>
2019-09-09 20:33:19 -0500

    Adding one_post template

--- Personal_Blog/Post/views.py
+++ Personal_Blog/Post/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views import View
 
-# Create your views here.
+from .models import Post
+
+
+class OnePost(View):
+    """
+        Displays just one post
+    """
+    template = 'Post/one_post.html'
+    context = {}
+
+    def get(self, request, post_id):
+        """
+            GET in one post
+        """
+        # post = Post.objects.get(id=post_id)
+        post = get_object_or_404(Post, id=post_id)
+        self.context['post'] = post
+
+        self.context['title'] = str(post)
+
+        return render(request, self.template, self.context)
