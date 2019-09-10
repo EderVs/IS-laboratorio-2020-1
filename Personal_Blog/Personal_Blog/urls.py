1dfd6547159ff1c5e32c130d0abe153ea85dd360
edervs <eedervs.97@gmail.com>
2019-09-09 20:33:19 -0500

    Adding one_post template

--- Personal_Blog/Personal_Blog/urls.py
+++ Personal_Blog/Personal_Blog/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls')),
+    path('posts/', include('Post.urls')),
 ]
--- Personal_Blog/Post/models.py
+++ Personal_Blog/Post/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     """
     title = models.CharField(max_length=200, null=False)
     paragraph = models.TextField(null=False)
-    date = models.DateTimeField(default=timezone.now(), null=False)
+    date = models.DateTimeField(default=timezone.now, null=False)
 
     def __str__(self):
         return str(self.id) + " " + self.title
