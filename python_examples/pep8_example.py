526145911a1b92a1de285a7ac64cdca5c82ad6a4
edervs <eedervs.97@gmail.com>
2019-09-25 17:40:48 -0500

    Adding Pep8 examples

---
+++ python_examples/pep8_example.py
@@ -0,0 +1,21 @@
+"""
+    File description
+"""
+
+
+# 2 blank spaces between classes
+# Class name in CamelCase
+class ClassExample:
+    """
+        Class description
+    """
+
+    # 1 blank spaces min between method inside a class
+    # Variable and method names in underscore_case
+    def method_example(self):
+        """
+            Method description
+        """
+        # Recomendation of 4 spaces by identation
+        self.attribute_example = 4 # This is other example of comment
+        variable_example = 5       # The length of a line is below 79 chars
