d8e5ce9d540ef5ba71bd938919ddf7bc216808c4
edervs <eedervs.97@gmail.com>
2019-10-27 21:27:37 -0600

    Adding header

--- Personal_Blog/Post/forms.py
+++ Personal_Blog/Post/forms.py
@@ -2,13 +2,18 @@ from django import forms
 from django.forms import ModelForm
 from .models import Post
 
-class PostForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=100)
-    paragraph = forms.CharField(label='Paragraph', widget=forms.Textarea)
+# class PostForm(forms.Form):
+#    title = forms.CharField(label='Title', max_length=100)
+#    paragraph = forms.CharField(label='Paragraph', widget=forms.Textarea)
+#    image = forms.ImageField(label='Image')
+
+
+class PostForm(ModelForm):
+    """
+        This is the form to create a post
+    """
     image = forms.ImageField(label='Image')
 
-
-#class PostForm(ModelForm):
-#    class Meta:
-#        model = Post
-#        fields = ['title', 'paragraph', 'image']
+    class Meta:
+        model = Post
+        fields = ['title', 'paragraph']
