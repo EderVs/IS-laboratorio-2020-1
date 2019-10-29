f075e258330642f57c9804f29a77b8a31c3f1d18
edervs <eedervs.97@gmail.com>
2019-10-28 20:47:50 -0600

    Adding Subscriber Model

--- Personal_Blog/Home/models.py
+++ Personal_Blog/Home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Subscriber(models.Model):
+    """
+        Subscriber to blog's posts
+    """
+    email = models.EmailField(
+        max_length=200, null=False, unique=True
+    )
+
+    def __str__(self):
+        return str(self.email)
